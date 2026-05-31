ad9dbba4b47fe6acfe6893d3518c904a7b43dbfb
sobhan <sobhanjafarii87@gmail.com>
2026-05-31 10:15:15 +0330

    [ADD] Board list component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
-import {ReactNode} from "react";
+import { ReactNode } from "react";
+import BoardList from "@/features/board/Board-List";
 
 export default function Home(): ReactNode {
-    return (
-        <div>
-            <h1>Hello world!</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className={"mb-2"}>Boards</h1>
+
+      <BoardList />
+    </div>
+  );
 }
