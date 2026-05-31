571fea4c103415655516b567b10fad30d1440070
sobhan <sobhanjafarii87@gmail.com>
2026-05-31 14:11:36 +0330

    [ADD] list and list item type

---
+++ src/types/List-Item.ts
@@ -0,0 +1,1 @@
+export type ListItemType = { id: string; title: string };
---
+++ src/types/List.ts
@@ -0,0 +1,3 @@
+import { ListItemType } from "./List-Item";
+
+export type ListType = { id: string; title: string; items: ListItemType[] };
