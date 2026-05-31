279ad0f49cd161c1f14d6f22b0074e1f1d56240e
sobhan <sobhanjafarii87@gmail.com>
2026-05-31 19:55:12 +0330

    [UPDATE] Icon Button

--- src/components/Icon-Button.tsx
+++ src/components/Icon-Button.tsx
@@ -1,12 +1,15 @@
 import { ComponentProps, ReactNode } from "react";
 
 export default function IconButton({
-   children,
-   ...anotherProps
+  children,
+  ...anotherProps
 }: ComponentProps<"button">): ReactNode {
-   return (
-      <button {...anotherProps} className="cursor-pointer active:opacity-10">
-         {children}
-      </button>
-   );
+  return (
+    <button
+      {...anotherProps}
+      className="cursor-pointer active:opacity-10 p-1 rounded-full hover:bg-gray-300"
+    >
+      {children}
+    </button>
+  );
 }
