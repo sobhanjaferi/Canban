fbcfbd8d8c14ccd9d5481264eb0b0bd28b1cc63e
sobhan <sobhanjafarii87@gmail.com>
2026-06-02 13:32:31 +0330

    [UPDATE] board detail list and board list

--- src/features/board/Board-List.tsx
+++ src/features/board/Board-List.tsx
@@ -2,38 +2,32 @@ import { ReactElement } from "react";
 import BoardBox from "@/features/board/Board-Box";
 
 export default function BoardList(): ReactElement {
-   return (
-      <div
-         className={
-            "grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 p-5 sm:p-0"
-         }
-      >
-         <BoardBox
-            id={1}
-            title={"board 1"}
-            color={"green"}
-            content={
-               "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-            }
-         />
+  return (
+    <div
+      className={
+        "grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-5 p-5 sm:p-0"
+      }
+    >
+      <BoardBox
+        id={1}
+        title={"board 1"}
+        color={"green"}
+        content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+      />
 
-         <BoardBox
-            id={2}
-            title={"board 2"}
-            color={"yellow"}
-            content={
-               "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-            }
-         />
+      <BoardBox
+        id={2}
+        title={"board 2"}
+        color={"yellow"}
+        content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+      />
 
-         <BoardBox
-            id={3}
-            title={"board 3"}
-            color={"blue"}
-            content={
-               "Lorem ipsum dolor sit amet, consectetur adipisicing elit."
-            }
-         />
-      </div>
-   );
+      <BoardBox
+        id={3}
+        title={"board 3"}
+        color={"blue"}
+        content={"Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
+      />
+    </div>
+  );
 }
--- src/features/board/Board-detail-list.tsx
+++ src/features/board/Board-detail-list.tsx
@@ -11,7 +11,7 @@ type Props = {
 
 export default function BoardDetailList({ lists, onClick }: Props): ReactNode {
   return (
-    <section className="flex justify-start items-start gap-3 overflow-auto h-150 p-3 sm:p-0">
+    <section className="flex justify-start items-start gap-3 overflow-auto p-3 sm:p-0 sm:pb-3">
       {lists.map((list) => (
         <BoardDetailBox key={list.id} list={list} onClick={onClick} />
       ))}
