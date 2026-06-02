8a2c87aad6d9716230748b0c0f0e30f8cd56bed7
sobhan <sobhanjafarii87@gmail.com>
2026-06-02 12:23:44 +0330

    [UPDATE]  Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -15,9 +15,9 @@ export default function Button({
       <button
          {...otherProps}
          className={`p-2.5 rounded-md text-xl cursor-pointer active:opacity-10 outline-0
-          ${color === "gray" && variant === "outline" && "bg-gray-200 text-gray-600 border-2 border-gray-500"}
-          ${color === "gray" && variant === "solid" && "bg-gray-500 text-white"}
-          ${color === "blue" && variant === "outline" && "bg-blue-200 border-2 border-blue-500 text-blue-600"}
+          ${color === "gray" && variant === "outline" && "bg-gray-400 text-gray-800 border-2 border-gray-600"}
+          ${color === "gray" && variant === "solid" && "bg-gray-400 text-white"}
+          ${color === "blue" && variant === "outline" && "bg-blue-200 border-2 border-blue-500 text-blue-700"}
           ${color === "blue" && variant === "solid" && "bg-blue-500 text-white"}
         `}
       >
