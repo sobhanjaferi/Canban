fb14f3fc9ee8526a9d52bb88b9f3f999571e3367
sobhan <sobhanjafarii87@gmail.com>
2026-06-20 15:30:20 +0330

    [REFACTOR] create logic

--- src/context/ListsContext.ts
+++ src/context/ListsContext.ts
@@ -6,14 +6,14 @@ import { createContext } from "react";
 
 type ListsContextType = {
   lists: ListType[];
-  create: (listId: string, item: ListItemType) => void;
+  create: (listIndex: number, item: ListItemType) => void;
   remove: (listId: string, itemId: string) => void;
-  move: (fromListId: string, itemId: string, toListId: string) => void;
+  // move: (fromListId: string, itemId: string, toListId: string) => void;
 };
 
 export const ListsContext = createContext<ListsContextType>({
   lists: [],
   create: () => {},
   remove: () => {},
-  move: () => {},
+  // move: () => {},
 });
